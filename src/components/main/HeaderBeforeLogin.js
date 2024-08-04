a12de5e79fec5400064d585f444a3b949d9090d2
JinhyunMSQ <sanul0319@gmail.com>
2024-08-04 12:55:26 +0900

    fix: 헤더네브바의 링크이동 기능이  정상작동하도록 코드수정중

--- src/components/main/HeaderBeforeLogin.js
+++ src/components/main/HeaderBeforeLogin.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './HeaderBeforeLogin.css';
+import { Link, useNavigate } from 'react-router-dom';
+import logo from '../../assets/images/image.png';
 
 const HeaderBeforeLogin = () => {
   return (
     <header className="header">
       <div className="logo">JOLLY JOURNEY</div>
       <nav className="nav">
-        <ul>
-          <li><button className="nav-button">육아 스트레스 정보</button></li>
-          <li><button className="nav-button">나의 기록</button></li>
+      <ul className="navbar-menu">
+          <li><Link to="/info">육아 스트레스 정보</Link></li>
+          <li><Link to="/record">나의 기록</Link></li>
         </ul>
       </nav>
       <div className="search-container">
